d76667cd40b989b47bbd08ef5323c70b845b5927
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-03-29 11:21:46 -0300

    29.03.21 - App.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json()); // json de entrada no body
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
 
